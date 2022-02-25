0ff3a107dd4f30bcb67430722bfe712096b11c5f
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-02-25 19:16:30 +0100

    Added challenge #2

--- #2 Live Input Feedback/script.js
+++ #2 Live Input Feedback/script.js
@@ -1,10 +1,9 @@
 const emailInput = document.querySelector('#email');
+const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,6})(\.[a-z]{2,6})?$/;
 
 emailInput.addEventListener('keyup', () => {
     validate(emailInput, emailRegex);
 });
-
-const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,6})(\.[a-z]{2,6})?$/;
 
 const validate = (input, regex) => {
     const match = regex.test(input.value);
