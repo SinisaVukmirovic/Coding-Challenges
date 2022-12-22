431845735c3679054a1468bb78a4e3937e093307
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-12-22 21:32:48 +0100

    Added Chal 4 and 5 my solutions

---
+++ 5. LoremIpsum Flavour/script.js
@@ -0,0 +1,49 @@
+
+// dom elements
+const buttons = document.querySelectorAll('button')
+const textarea = document.querySelector('textarea')
+const output = document.querySelector('.output')
+
+// flavours
+const flavours = {
+    ninja: ['dojo', 'shogun', 'shinobi', 'samuri', 'shuriken'],
+    pokemon: ['pika', 'pokeball', 'pokedex', 'evolve', 'ash', 'gym'],
+    space: ['universe', 'galaxy', 'telescope', 'comet', 'stars'],
+}
+
+// functions
+const flavourize = (inputText, flavour) => {
+    const chosenFlavour = flavours[flavour];
+    // console.log(chosenFlavour)
+    const lenghtOfFlavourArray = [...chosenFlavour].length;
+    // console.log(lenghtOfFlavourArray)
+    function randomize() {
+        return Math.floor(Math.random() * lenghtOfFlavourArray)
+    }
+
+    const text = inputText.split(' ');
+    // console.log(text)
+    for (let i = 2; i < text.length; i = i + 3) {
+        text[i] = chosenFlavour[randomize()]
+    }
+    
+    updateOutput(text);
+}
+
+const updateOutput = (text) => {
+    text = [...text].join(', ');
+    output.textContent = text;
+}
+
+// event listener
+buttons.forEach(b => {
+    b.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const textToFlavorize = textarea.value;
+        const clickedFlavour = e.target.dataset.flavour;
+
+        flavourize(textToFlavorize, clickedFlavour)
+    })
+})
+
