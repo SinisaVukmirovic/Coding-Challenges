f60a742bc2b435230b20604619135241a2ae97b4
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-12-23 20:09:17 +0100

    Added Ninja's solution to challenge 5

--- 5. LoremIpsum Flavour/script.js
+++ 5. LoremIpsum Flavour/script.js
@@ -1,4 +1,3 @@
-
 // dom elements
 const buttons = document.querySelectorAll('button')
 const textarea = document.querySelector('textarea')
@@ -6,44 +5,94 @@ const output = document.querySelector('.output')
 
 // flavours
 const flavours = {
-    ninja: ['dojo', 'shogun', 'shinobi', 'samuri', 'shuriken'],
-    pokemon: ['pika', 'pokeball', 'pokedex', 'evolve', 'ash', 'gym'],
-    space: ['universe', 'galaxy', 'telescope', 'comet', 'stars'],
+  ninja: ['dojo', 'shogun', 'shinobi', 'samuri', 'shuriken'],
+  pokemon: ['pika', 'pokeball', 'pokedex', 'evolve', 'ash', 'gym'],
+  space: ['universe', 'galaxy', 'telescope', 'comet', 'stars'],
 }
 
 // functions
 const flavourize = (inputText, flavour) => {
-    const chosenFlavour = flavours[flavour];
-    // console.log(chosenFlavour)
-    const lenghtOfFlavourArray = [...chosenFlavour].length;
-    // console.log(lenghtOfFlavourArray)
-    function randomize() {
-        return Math.floor(Math.random() * lenghtOfFlavourArray)
-    }
+    const textArray = inputText.split(' ');
 
-    const text = inputText.split(' ');
-    // console.log(text)
-    for (let i = 2; i < text.length; i = i + 3) {
-        text[i] = chosenFlavour[randomize()]
+    for (let i = 0; i < textArray.length; i++) {
+        if (i % 3 === 0) {
+            const random = Math.floor(Math.random() * flavour.length);
+            textArray[i] = flavour[random];
+        }
     }
-    
-    updateOutput(text);
+    return textArray.join(' ');
 }
 
 const updateOutput = (text) => {
-    text = [...text].join(', ');
     output.textContent = text;
 }
 
 // event listener
 buttons.forEach(b => {
-    b.addEventListener('click', (e) => {
-        e.preventDefault();
+  b.addEventListener('click', (e) => {
+    e.preventDefault();
 
-        const textToFlavorize = textarea.value;
-        const clickedFlavour = e.target.dataset.flavour;
+    if (textarea.value) {
+        const flavour = e.target.dataset.flavour;
 
-        flavourize(textToFlavorize, clickedFlavour)
-    })
+        const text = flavourize(textarea.value, flavours[flavour]);
+
+        updateOutput(text);
+    }
+
+  })
 })
 
+
+
+// =====================================================================
+// MY SOLUTION
+
+// // dom elements
+// const buttons = document.querySelectorAll('button')
+// const textarea = document.querySelector('textarea')
+// const output = document.querySelector('.output')
+
+// // flavours
+// const flavours = {
+//     ninja: ['dojo', 'shogun', 'shinobi', 'samuri', 'shuriken'],
+//     pokemon: ['pika', 'pokeball', 'pokedex', 'evolve', 'ash', 'gym'],
+//     space: ['universe', 'galaxy', 'telescope', 'comet', 'stars'],
+// }
+
+// // functions
+// const flavourize = (inputText, flavour) => {
+//     const chosenFlavour = flavours[flavour];
+//     // console.log(chosenFlavour)
+//     const lenghtOfFlavourArray = [...chosenFlavour].length;
+//     // console.log(lenghtOfFlavourArray)
+//     function randomize() {
+//         return Math.floor(Math.random() * lenghtOfFlavourArray)
+//     }
+
+//     const text = inputText.split(' ');
+//     // console.log(text)
+//     for (let i = 2; i < text.length; i = i + 3) {
+//         text[i] = chosenFlavour[randomize()];
+//     }
+    
+//     updateOutput(text);
+// }
+
+// const updateOutput = (text) => {
+//     text = [...text].join(', ');
+
+//     output.textContent = text;
+// }
+
+// // event listener
+// buttons.forEach(b => {
+//     b.addEventListener('click', (e) => {
+//         e.preventDefault();
+
+//         const textToFlavorize = textarea.value;
+//         const clickedFlavour = e.target.dataset.flavour;
+
+//         flavourize(textToFlavorize, clickedFlavour)
+//     })
+// })
